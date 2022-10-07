25bc3d2dad2ed0eb8e6230c7362ca415f08a9180
tylim <limteikyee@gmail.com>
2022-10-07 17:21:09 +0800

    encapsulate api

---
+++ packages/ui/src/api/index.ts
@@ -0,0 +1,8 @@
+export * as nodesApi from './nodes';
+export * as oauth2Api from './oauth2';
+export * as walletsApi from './wallets';
+export * as webhooksApi from './webhooks';
+export * as workflowsApi from './workflows';
+export * as contractsApi from './contracts';
+export * as credentialApi from './credential';
+export * as executionsApi from './executions';
