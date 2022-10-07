6350efd9acbc79579e5809b3f40ef7f974a21297
tylim <limteikyee@gmail.com>
2022-10-07 08:22:38 +0800

    convert client.js credential.js execution.js and node.js

--- packages/ui/src/api/client.ts
+++ packages/ui/src/api/client.ts
@@ -1,11 +1,9 @@
 import axios from 'axios';
 import { baseURL } from 'store/constant';
 
-const apiClient = axios.create({
+export const client = axios.create({
     baseURL: `${baseURL}/api/v1`,
     headers: {
         'Content-type': 'application/json'
     }
 });
-
-export default apiClient;
---
+++ packages/ui/src/api/credential.ts
@@ -0,0 +1,14 @@
+import { client } from './client';
+import { ICredential } from 'outerbridge-components';
+
+export const getCredentials = (nodeCredentialName: string) => client.get('/credentials', { params: { nodeCredentialName } });
+
+export const getCredentialParams = (name: string) => client.get(`/node-credentials/${name}`);
+
+export const getSpecificCredential = (id: string, isEncrypted: boolean) => client.get(`/credentials/${id}`, { params: { isEncrypted } });
+
+export const createNewCredential = (credentialBody: ICredential) => client.post(`/credentials`, credentialBody);
+
+export const updateCredential = (id: string, credentialBody: ICredential) => client.put(`/credentials/${id}`, credentialBody);
+
+export const deleteCredential = (id: string) => client.delete(`/credentials/${id}`);
