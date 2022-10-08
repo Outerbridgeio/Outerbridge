ffed124c9c1899a14a30cbaebbc79b8fbcf1ef3c
tylim <limteikyee@gmail.com>
2022-10-08 23:06:10 +0800

    1. simplify yarn run to yarn 2. temporary disable eslint-disable-next-line @typescript-eslint/no-var-requires in gulpfile.js

--- packages/components/gulpfile.js
+++ packages/components/gulpfile.js
@@ -1,4 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+// // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { src, dest } = require('gulp')
 
 function copyIcons() {
