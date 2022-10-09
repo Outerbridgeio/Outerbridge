bb9d5ba72539fc3cc81a2dedc51cd79c52077ac7
tylim <limteikyee@gmail.com>
2022-10-10 00:01:41 +0800

    merge index with store

--- packages/ui/src/store/index.ts
+++ packages/ui/src/store/index.ts
@@ -1,3 +1,10 @@
-export * from './store';
+import { legacy_createStore as createStore } from 'redux';
+import reducer from './reducer';
+
+// ==============================|| REDUX - MAIN STORE ||============================== //
+
+export const store = createStore(reducer);
+export const persister = 'Free';
+
 export * as actions from './actions';
 export * as constant from './constant';
--- packages/ui/src/store/store.ts
+++
@@ -1,7 +0,0 @@
-import { legacy_createStore as createStore } from 'redux';
-import reducer from './reducer';
-
-// ==============================|| REDUX - MAIN STORE ||============================== //
-
-export const store = createStore(reducer);
-export const persister = 'Free';
