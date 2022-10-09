306ac5d0d629fc3e272ef054bef44ca04cc84d38
tylim <limteikyee@gmail.com>
2022-10-10 02:34:53 +0800

    replace no-unused-var with unused-imports

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,9 +14,12 @@ module.exports = {
     },
     parser: '@typescript-eslint/parser',
     ignorePatterns: ['**/node_modules', '**/dist', '**/build'],
+    plugins: ['unused-imports'],
     rules: {
         '@typescript-eslint/explicit-module-boundary-types': 'off',
-        'no-unused-vars': 'warn',
+        'no-unused-vars': 'off',
+        'unused-imports/no-unused-imports': 'warn',
+        'unused-imports/no-unused-vars': ['warn', { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' }],
         'no-undef': 'off',
         'no-console': [process.env.CI ? 'error' : 'warn', { allow: ['warn', 'error', 'info'] }],
         'prettier/prettier': 'error'
