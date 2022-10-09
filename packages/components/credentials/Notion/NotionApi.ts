869eb92f6b759f3694a2e714dabc17069a298bfe
tylim <limteikyee@gmail.com>
2022-10-10 04:57:48 +0800

    format notion and notion api

--- packages/components/credentials/Notion/NotionApi.ts
+++ packages/components/credentials/Notion/NotionApi.ts
@@ -1,24 +1,24 @@
-import { INodeParams, INodeCredential } from '../../src/Interface';
+import { INodeParams, INodeCredential } from '../../src/Interface'
 
 class NotionApi implements INodeCredential {
-  name: string;
-  version: number;
-  credentials: INodeParams[];
+    name: string
+    version: number
+    credentials: INodeParams[]
 
-  constructor() {
-    this.name = 'notionApi';
-    this.version = 1.0;
-    this.credentials = [
-      {
-        label: 'Internal Integration Token',
-        name: 'integrationToken',
-        type: 'string',
-        default: '',
-        description:
-          'Get your <a target="_blank" href="https://www.notion.so/my-integrations">Internal Integration Token</a> for your workspace',
-      },
-    ];
-  }
+    constructor() {
+        this.name = 'notionApi'
+        this.version = 1.0
+        this.credentials = [
+            {
+                label: 'Internal Integration Token',
+                name: 'integrationToken',
+                type: 'string',
+                default: '',
+                description:
+                    'Get your <a target="_blank" href="https://www.notion.so/my-integrations">Internal Integration Token</a> for your workspace'
+            }
+        ]
+    }
 }
 
-module.exports = { credClass: NotionApi };
+module.exports = { credClass: NotionApi }
