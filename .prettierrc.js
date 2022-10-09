164ef0b07a6b3bfc98cf0cf5a42dfbe61d60a551
tylim <limteikyee@gmail.com>
2022-10-10 03:07:44 +0800

    remove prettier options with default value

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,11 +1,9 @@
 module.exports = {
-    bracketSpacing: true,
     printWidth: 140,
     singleQuote: true,
     jsxSingleQuote: true,
     trailingComma: 'none',
     tabWidth: 4,
-    useTabs: false,
     semi: false,
     endOfLine: 'auto'
 }
