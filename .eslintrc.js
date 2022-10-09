8f7c53bbc7ed1dad1aa19ac45c4897a7885c63a6
tylim <limteikyee@gmail.com>
2022-10-10 00:23:05 +0800

    1. remove `rimraf **/yarn.lock` 2. fix command precedence 3. more granular no console rule, allow console.warn, console.error and console.info 4. change console.log in service worker to console.info

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,7 +24,7 @@ module.exports = {
         {
             files: ['packages/ui/**'],
             rules: {
-                'no-console': 'warn'
+                'no-console': ['warn', { allow: ['warn', 'error', 'info'] }]
             }
         }
     ]
