f639729f0357daf278034f9391e03df38208f12c
tylim <limteikyee@gmail.com>
2022-10-09 08:18:27 +0800

    change eslint and prettier config files to js files so we can write comment in it

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react/jsx-runtime',
+        'plugin:react-hooks/recommended',
+        'plugin:jsx-a11y/recommended',
+        'plugin:prettier/recommended'
+    ],
+    settings: {
+        react: {
+            version: 'detect'
+        }
+    },
+    parser: '@typescript-eslint/parser',
+    ignorePatterns: ['**/node_modules', '**/dist', '**/build'],
+    rules: {
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        'no-unused-vars': 'warn',
+        'no-undef': 'off',
+        'no-console': 'warn',
+        'prettier/prettier': ['error']
+    }
+}
---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    bracketSpacing: true,
+    printWidth: 140,
+    singleQuote: true,
+    jsxSingleQuote: true,
+    trailingComma: 'none',
+    tabWidth: 4,
+    useTabs: false,
+    semi: false,
+    endOfLine: 'auto'
+}
