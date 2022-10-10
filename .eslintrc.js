f75633ba67280cfdb547b101f1bdbb219048e466
tylim <limteikyee@gmail.com>
2022-10-10 09:18:21 +0800

    1. eslint now lint and format json files 2. format all json files

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,7 @@ module.exports = {
         }
     },
     parser: '@typescript-eslint/parser',
-    ignorePatterns: ['**/node_modules', '**/dist', '**/build'],
+    ignorePatterns: ['**/node_modules', '**/dist', '**/build', '**/package-lock.json'],
     plugins: ['unused-imports'],
     rules: {
         '@typescript-eslint/explicit-module-boundary-types': 'off',
