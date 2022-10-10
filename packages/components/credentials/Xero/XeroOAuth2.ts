9e562b9dd95b19a9b5b09e0bfbfaa7292f47dfe3
Christopher Vestman <christophervestman@gmail.com>
2022-10-10 23:35:10 +0200

    feat: added Xero logo and OAuth2 flow

---
+++ packages/components/credentials/Xero/XeroOAuth2.ts
@@ -0,0 +1,54 @@
+import { INodeParams, INodeCredential } from '../../src/Interface';
+
+class XeroOAuth2 implements INodeCredential {
+    name: string;
+    version: number;
+    credentials: INodeParams[];
+
+    constructor() {
+        this.name = 'xeroOAuth2Api';
+        this.version = 1.0;
+        this.credentials = [
+            {
+                label: 'Client ID',
+                name: 'clientID',
+                type: 'string',
+                default: '',
+                description: 'How to get Client ID & Secret: https://developer.xero.com/documentation/guides/oauth2/client-credentials/'
+            },
+            {
+                label: 'Client Secret',
+                name: 'clientSecret',
+                type: 'password',
+                default: '',
+                description: 'How to get Client ID & Secret: https://developer.xero.com/documentation/guides/oauth2/client-credentials/'
+            },
+            {
+                label: 'Authorization URL',
+                name: 'authUrl',
+                type: 'string',
+                default: 'https://login.xero.com/identity/connect/authorize'
+            },
+            {
+                label: 'Access Token Url',
+                name: 'accessTokenUrl',
+                type: 'string',
+                default: 'https://identity.xero.com/connect/token'
+            },
+            {
+                label: 'Authorization URL Parameters',
+                name: 'authorizationURLParameters',
+                type: 'string',
+                default: 'response_type=code'
+            },
+            {
+                label: 'Scope',
+                name: 'scope',
+                type: 'string',
+                default: `["openid", "profile", "email", "accounting.settings", "accounting.reports.read", "accounting.journals.read", "accounting.contacts", "accounting.attachments", "accounting.transactions", "offline_access"]`
+            }
+        ];
+    }
+}
+
+module.exports = { credClass: XeroOAuth2 };
