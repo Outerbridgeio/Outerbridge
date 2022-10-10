2a3204c45a65c0f52b9034033b9d581754d4e470
tylim <limteikyee@gmail.com>
2022-10-11 03:33:03 +0800

    bug fix: replace process.NODE_ENV with process.env.NODE_ENV

--- packages/ui/src/store/constant.js
+++ packages/ui/src/store/constant.js
@@ -2,7 +2,7 @@
 export const gridSpacing = 3
 export const drawerWidth = 260
 export const appDrawerWidth = 320
-export const baseURL = process.NODE_ENV === 'production' ? window.location.origin : window.location.origin.replace(':8080', ':3000')
+export const baseURL = process.env.NODE_ENV === 'production' ? window.location.origin : window.location.origin.replace(':8080', ':3000')
 export const networkExplorers = {
     homestead: 'https://etherscan.io',
     rinkeby: 'https://rinkeby.etherscan.io',
