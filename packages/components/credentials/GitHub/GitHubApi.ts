3c664744775203623b1e9e37f5df2b1e339c7d4f
chungyau97 <chungyau97@gmail.com>
2022-10-31 22:30:36 +0700

    lint fix

--- packages/components/credentials/GitHub/GitHubApi.ts
+++ packages/components/credentials/GitHub/GitHubApi.ts
@@ -1,24 +1,23 @@
-import { INodeParams, INodeCredential } from "../../src/Interface";
+import { INodeParams, INodeCredential } from '../../src/Interface'
 
 class GitHubApi implements INodeCredential {
-  name: string;
-  version: number;
-  credentials: INodeParams[];
+    name: string
+    version: number
+    credentials: INodeParams[]
 
-  constructor() {
-    this.name = "gitHubApi";
-    this.version = 1.0;
-    this.credentials = [
-      {
-        label: "Access Token",
-        name: "accessToken",
-        type: "string",
-        default: "",
-        description:
-          '<a target="_blank" href="https://github.com/settings/tokens">Register GitHub and get your access token.</a>"',
-      },
-    ];
-  }
+    constructor() {
+        this.name = 'gitHubApi'
+        this.version = 1.0
+        this.credentials = [
+            {
+                label: 'Access Token',
+                name: 'accessToken',
+                type: 'string',
+                default: '',
+                description: '<a target="_blank" href="https://github.com/settings/tokens">Register GitHub and get your access token.</a>"'
+            }
+        ]
+    }
 }
 
-module.exports = { credClass: GitHubApi };
+module.exports = { credClass: GitHubApi }
