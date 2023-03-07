0704112bb721040a3c4f2b1a46357d1d44b93f29
Ozgur Sallanci <ozgur.sallanci@gmail.com>
2023-03-07 20:26:32 +0300

    mui packages and react-test libs are updated

--- packages/ui/src/index.js
+++ packages/ui/src/index.js
@@ -1,36 +1,28 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import { store } from 'store'
+
+// style + assets
+import 'assets/scss/style.scss'
+
 // third party
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { SnackbarProvider } from 'notistack'
 import ConfirmContextProvider from 'store/context/ConfirmContextProvider'
 
-// project imports
-import * as serviceWorker from 'serviceWorker'
-import App from 'App'
-import { store } from 'store'
-
-import { createRoot } from 'react-dom/client'
-const container = document.getElementById('root')
-const root = createRoot(container) // createRoot(container!) if you use TypeScript
-
-// style + assets
-import 'assets/scss/style.scss'
-
-// ==============================|| REACT DOM RENDER  ||============================== //
-
-root.render(
-    <Provider store={store}>
-        <BrowserRouter>
-            <SnackbarProvider>
-                <ConfirmContextProvider>
-                    <App />
-                </ConfirmContextProvider>
-            </SnackbarProvider>
-        </BrowserRouter>
-    </Provider>
+ReactDOM.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <BrowserRouter>
+                <SnackbarProvider>
+                    <ConfirmContextProvider>
+                        <App />
+                    </ConfirmContextProvider>
+                </SnackbarProvider>
+            </BrowserRouter>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 )
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
