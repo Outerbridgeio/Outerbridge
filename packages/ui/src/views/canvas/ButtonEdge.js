9e81885a940a8e9b9267660dc867c6c63bdba83a
Ozgur Sallanci <ozgur.sallanci@gmail.com>
2023-03-10 10:30:06 +0300

    react-flow package upgraded to v11

--- packages/ui/src/views/canvas/ButtonEdge.js
+++ packages/ui/src/views/canvas/ButtonEdge.js
@@ -1,4 +1,4 @@
-import { getBezierPath, getEdgeCenter, EdgeText } from 'react-flow-renderer'
+import { getBezierPath } from 'reactflow'
 import PropTypes from 'prop-types'
 import { useDispatch } from 'react-redux'
 import { REMOVE_EDGE } from 'store/actions'
@@ -7,21 +7,14 @@ import './index.css'
 
 const foreignObjectSize = 40
 
-const ButtonEdge = ({ id, sourceX, sourceY, targetX, targetY, sourcePosition, targetPosition, style = {}, data, markerEnd }) => {
-    const edgePath = getBezierPath({
+const ButtonEdge = ({ id, sourceX, sourceY, targetX, targetY, sourcePosition, targetPosition, style = {}, markerEnd }) => {
+    const [edgePath, labelX, labelY] = getBezierPath({
         sourceX,
         sourceY,
         sourcePosition,
         targetX,
         targetY,
         targetPosition
-    })
-
-    const [edgeCenterX, edgeCenterY] = getEdgeCenter({
-        sourceX,
-        sourceY,
-        targetX,
-        targetY
     })
 
     const dispatch = useDispatch()
@@ -34,27 +27,16 @@ const ButtonEdge = ({ id, sourceX, sourceY, targetX, targetY, sourcePosition, ta
     return (
         <>
             <path id={id} style={style} className='react-flow__edge-path' d={edgePath} markerEnd={markerEnd} />
-            {data && data.label && (
-                <EdgeText
-                    x={sourceX + 10}
-                    y={sourceY + 10}
-                    label={data.label}
-                    labelStyle={{ fill: 'black' }}
-                    labelBgStyle={{ fill: 'transparent' }}
-                    labelBgPadding={[2, 4]}
-                    labelBgBorderRadius={2}
-                />
-            )}
             <foreignObject
                 width={foreignObjectSize}
                 height={foreignObjectSize}
-                x={edgeCenterX - foreignObjectSize / 2}
-                y={edgeCenterY - foreignObjectSize / 2}
+                x={labelX - foreignObjectSize / 2}
+                y={labelY - foreignObjectSize / 2}
                 className='edgebutton-foreignobject'
                 requiredExtensions='http://www.w3.org/1999/xhtml'
             >
                 <div>
-                    <button type='button' className='edgebutton' onClick={(event) => onEdgeClick(event, id)}>
+                    <button className='edgebutton' onClick={(event) => onEdgeClick(event, id)}>
                         ×
                     </button>
                 </div>
